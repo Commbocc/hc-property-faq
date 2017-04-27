be04227fb37e167ff887c1d2e214095c40d8654f
Kevin McCarthy <mccarthyk@hcflgov.net>
2017-04-27 14:18:03 -0400

    continued IE testing

--- assets/propfaq/init.js
+++ assets/propfaq/init.js
@@ -7,60 +7,77 @@ $(function() {
 		Views: {}
 	};
 
-	PropFAQ.Routers.MainRouter = Backbone.Router.extend({
-		routes: {
-			":id": "individual",
-			"*actions": "all"
-		},
+	PropFAQ.dataDir = window.propfaq_dir_path || '/hc-property-faq/assets/propfaq';
 
-		initialize: function(options) {
-			PropFAQ.dataDir = PropFAQ.dataDir || options.dir_path;
-		},
+	define([
+		PropFAQ.dataDir+"/search.js",
+		PropFAQ.dataDir+"/results.js",
+		PropFAQ.dataDir+"/errors.js"
+	], function() {
 
-		all: function() {
-			// all Property FAQ services
-			console.log('all');
 
-			require([
-				"esri/tasks/Locator",
-			], function(Locator) {
+		PropFAQ.Routers.MainRouter = Backbone.Router.extend({
+			routes: {
+				":id": "individual",
+				"*actions": "all"
+			},
 
-				var hcLocator = new Locator({
-					url: "https://maps.hillsboroughcounty.org/arcgis/rest/services/Geocoding/DBO_composite_address_locator/GeocodeServer"
-				});
+			initialize: function(options) {
+				// PropFAQ.dataDir = PropFAQ.dataDir || options.dir_path;
+			},
 
-				hcLocator.addressToLocations({
-					address: { SingleLine: '805 sandcastle' },
-					maxLocations: 1
-				}).then(function(response) {
-					console.log(response)
-				});
+			all: function() {
+				// all Property FAQ services
+				console.log('all');
 
-			});
-
-		},
-
-		individual: function(id) {
-			// individual Property FAQ service
-			console.log('individual: ' + id);
-
-			require([
-				PropFAQ.dataDir+"/"+id+"/init.js",
-				PropFAQ.dataDir+"/search.js",
-				PropFAQ.dataDir+"/results.js",
-				PropFAQ.dataDir+"/errors.js"
-			], function(IndividualPropFaqModel) {
 				var propfaq_search_view = new PropFAQ.Views.SearchView;
 
 				propfaq_search_view.model.on('change:folio', function(that) {
-					var model = new IndividualPropFaqModel(that.get('folio'));
-					model.set('search_str', that.get('candidate').address);
-					var propfaq_results_view = new PropFAQ.Models.ResultsView({model: model});
+					console.log(that);
 				});
-			});
 
-		}
+				// require([
+				// 	"esri/tasks/Locator",
+				// 	"esri/tasks/QueryTask",
+				// 	"esri/tasks/support/Query"
+				// ], function(Locator, QueryTask, Query) {
+				//
+				// 	var hcLocator = new Locator({
+				// 		url: "https://maps.hillsboroughcounty.org/arcgis/rest/services/Geocoding/DBO_composite_address_locator/GeocodeServer"
+				// 	});
+				//
+				// 	hcLocator.addressToLocations({
+				// 		address: { SingleLine: '805 sandcastle' },
+				// 		maxLocations: 1
+				// 	}).then(function(response) {
+				// 		console.log(response)
+				// 	});
+				//
+				// });
+
+			},
+
+			individual: function(id) {
+				// individual Property FAQ service
+				console.log('individual: ' + id);
+
+				// require([
+				// 	PropFAQ.dataDir+"/"+id+"/init.js"
+				// ], function(IndividualPropFaqModel) {
+				// 	var propfaq_search_view = new PropFAQ.Views.SearchView;
+				//
+				// 	propfaq_search_view.model.on('change:folio', function(that) {
+				// 		var model = new IndividualPropFaqModel(that.get('folio'));
+				// 		model.set('search_str', that.get('candidate').address);
+				// 		var propfaq_results_view = new PropFAQ.Models.ResultsView({model: model});
+				// 	});
+				// });
+
+			}
+
+		});
+
+		return PropFAQ.Routers.MainRouter;
 
 	});
-
 });
